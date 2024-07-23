c5edb25ee0c61ebb9e22a10f9c75520b4b3a3d76
Lucas Magalhães <lucasmagalhaes.dev@gmail.com>
2024-07-23 20:08:36 -0300

    - Adiciona a página para visualizar os dados da pesquisa realizada

--- components/registrarPesquisas/visualizar.vue
+++ components/registrarPesquisas/visualizar.vue
@@ -1,14 +1,69 @@
 <template>
-    <h2>Visualizar Pesquisa</h2>
+    <v-row v-if="dadosPesquisa.dados != null">
+        <v-col cols="12" class="text-center text-h5 font-weight-bold cor1">
+            Dados da Pesquisa
+        </v-col>
+        <v-col cols="12" md="2">
+            <strong>Data: </strong> {{ dadosPesquisa.dados.created_at }}
+        </v-col>
+        <v-col cols="12" md="5">
+            <strong>Pesquisa:</strong> {{ dadosPesquisa.dados.pesquisa.titulo }}
+        </v-col>
+        <v-col cols="12" md="5">
+            <strong>Nome do Agente:</strong> {{ dadosPesquisa.dados.agente.nome }}
+        </v-col>
+        <v-col cols="12" md="4">
+            <strong>Tipo do entrevistado:</strong> {{ dadosPesquisa.dados.pesquisa.tipo_entrevistado == "A" ? "Anônimo" : "Cadastrado" }}
+        </v-col>
+        <v-col v-if="dadosPesquisa.dados.pesquisa.tipo_entrevistado == 'C'" cols="12" md="4">
+            <strong>Nome do entrevistado:</strong> {{ dadosPesquisa.dados.entrevistado.nome }}
+        </v-col>
+    </v-row>
+    <hr class="my-4" />
+    <v-row v-if="![null, undefined].includes(perguntas)">
+        <v-col cols="12" class="text-center text-h5 font-weight-bold cor1">
+            Perguntas
+        </v-col>
+        <v-col cols="12" md="6" v-for="(perg, index) in perguntas.data" :key="index">
+            <h3>{{ index + 1 }} - {{ perg.pergunta }}</h3>
+            <p>R: {{ perg.resposta }}</p>
+        </v-col>
+        <v-overlay
+            :model-value="carregando"
+            class="align-center justify-center"
+            >
+            <v-progress-circular
+                color="primary"
+                size="64"
+                indeterminate
+            ></v-progress-circular>
+        </v-overlay>
+    </v-row>
+    <v-row>
+        <v-col>
+            <v-btn prepend-icon="mdi-arrow-left" class="bg-cor5" @click="router.push({ path: `/pesquisas-realizadas` })">Voltar</v-btn>
+        </v-col>
+    </v-row>
 </template>
 
 <script setup>
-    let dadosPesquisa = reactive({ data:[] });
+    const route = useRoute();
+    const router = useRouter();
+    let dadosPesquisa = reactive({dados: null});
+    let perguntas = reactive({ data:[] });
+    const idPesquisa = route.params.id ?? null;
+    let carregando = ref(false);
     async function getDados(){
-        let pesquisas = await $fetch('http://localhost:8000/api/pesquisasRealizadas/23', {
+        carregando.value = true;
+        let pesquisas = await $fetch(`http://localhost:8000/api/pesquisasRealizadas/visualizar/${idPesquisa}`, {
             headers: { Authorization: `Bearer ${sessionStorage.getItem("pesquisaTokenUsuario")}` }
         });
-        console.log(pesquisas.dados);
+        
+        dadosPesquisa.dados = pesquisas.pesquisa.dados;
+        perguntas.data = pesquisas.pesquisa.listaPerguntas;
+
+        carregando.value = false;
+
     }
 
     getDados();
